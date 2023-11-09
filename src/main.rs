5b4c56e0d581ea21c9af6d658281330606e99af7
Robert Oswald <robert.oswald.osw@gmail.com>
2023-11-09 22:15:10 +0000

    old code from main removed

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,7 @@ fn accounting_csv_import() {
 
     let year: u16 = Input::new().with_prompt("Year").interact_text().unwrap();
     let month: u8 = Input::new().with_prompt("Month").interact_text().unwrap();
+    // TODO note
 
     accounting_input_month_from_csv(&path, year, month);
 }
@@ -111,87 +112,8 @@ fn investing_new_depot_entry() {
     investing_new_depot_element(name, DepotElement::default(InvestmentVariant::from_str(variants[selection]).unwrap()));
 }
 fn investing_set_comparisons() {
-    todo!();
+    todo!(); // TODO
 }
 fn investing_modify_savings_plan() {
-    todo!();
+    todo!(); // TODO
 }
-
-// TODO write own panic macro that does not output lines and compiler message (panic_release!)
-
-// fn main() {
-//     let args: Vec<String> = args().collect();
-
-//     match parse_task(&args) {
-//         CliTask::AccountingTableOutput => print_accounting_table(parse_args_for_table_output(&args)),
-//         CliTask::AccountingInputMonthFromCsv => {
-//             let (path, year_nr, month_nr) = parse_args_for_csv_input(&args);
-//             accounting_input_month_from_csv(&path, year_nr, month_nr);
-//         }
-//         CliTask::ManualAccountingInput => {
-//             let (income, expenses, month_nr, year_nr): (f64, f64, u8, u16) = parse_args_for_manual_input(&args);
-//             accounting_input_manual(income, expenses, month_nr, year_nr);
-//         }
-//         CliTask::ManualInvestingInput => {
-//             todo!()
-//         }
-//         CliTask::UnknownCommand => print_cmd_usage(),
-//         CliTask::WrongUsage => print_cmd_usage(),
-//     }
-// }
-
-// /// - Check which command arguments have been given and find out which task has to be done
-// /// - Correct Task is only returned if the correct amount of cmd arguments for this task have been provided, content is not checked however
-// fn parse_task(args: &Vec<String>) -> CliTask {
-//     match args.len() {
-//         1 | 2 => return CliTask::WrongUsage,
-//         _ => (),
-//     };
-
-//     match args[1].as_str() {
-//         "-o" => match args.len() - 2 {
-//             1 => return CliTask::AccountingTableOutput,
-//             _ => return CliTask::WrongUsage,
-//         },
-//         "--csv" | "-c" => match args.len() - 2 {
-//             3 => return CliTask::AccountingInputMonthFromCsv,
-//             _ => return CliTask::WrongUsage,
-//         },
-//         "-i" => match args.len() - 2 {
-//             4 => return CliTask::ManualAccountingInput,
-//             _ => return CliTask::WrongUsage,
-//         },
-//         _ => return CliTask::UnknownCommand,
-//     }
-// }
-
-// /// - try to parse the command line arguments for this task
-// /// - Returns `year: u16`
-// fn parse_args_for_table_output(args: &Vec<String>) -> u16 {
-//     match args[2].parse::<u16>() {
-//         Ok(year) => return year,
-//         Err(e) => panic!("{:?} could not be parsed as a u16: {}", args[2], e),
-//     }
-// }
-
-// /// Explain the user how to use this command
-// fn print_cmd_usage() -> ! {
-//     let args: Vec<String> = args().collect();
-//     let cmd = args.get(0).unwrap();
-
-//     println!("Usage:");
-//     println!("\t{} [ -c | -i | -o ]", cmd);
-//     println!("");
-//     println!("1. Provide new data to save for later use (overwrites existing data)");
-//     println!("  1.1 Extract income and expenses from a csv file and define the year and month to which the data should be assigned");
-//     println!("\t{} -csv  [file (string)]   [year (int)] [month (int)]", cmd);
-//     println!("\t{} -csv  path/to/file.csv      2023           7", cmd);
-//     println!("\t{} -csv 'path/to/file.csv'     2023           7", cmd);
-//     println!("");
-//     println!("2. Output table with calculated values for one year");
-//     println!("\t{} -o [year (int)]", cmd);
-//     println!("\t{} -o     2023    ", cmd);
-//     println!("");
-
-//     exit(0);
-// }
