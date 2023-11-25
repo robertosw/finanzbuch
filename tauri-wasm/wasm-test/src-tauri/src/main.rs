ceb84b6a315b838ec4b95d97f3e4ae7686d937d3
Robert Oswald <robert.oswald.osw@gmail.com>
2023-11-25 17:54:43 +0000

    added tauri rust + yew template

--- tauri-wasm/wasm-test/src-tauri/src/main.rs
+++ tauri-wasm/wasm-test/src-tauri/src/main.rs
@@ -3,20 +3,11 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String { format!("Hello, {}! You've been greeted from Rust!", name) }
-
-#[tauri::command]
-fn html_sample() -> String
-{
-    String::from(
-        "
-        <div class='test'></div>
-        ",
-    )
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main()
-{
+fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
