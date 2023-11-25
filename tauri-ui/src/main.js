c9c6d447398eeb720c89f4a1746865121f6e90ae
Robert Oswald <robert.oswald.osw@gmail.com>
2023-11-25 20:59:52 +0000

    tauri-js: base layout in css

--- tauri-ui/src/main.js
+++ tauri-ui/src/main.js
@@ -9,10 +9,10 @@ async function greet() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+  // greetInputEl = document.querySelector("#greet-input");
+  // greetMsgEl = document.querySelector("#greet-msg");
+  // document.querySelector("#greet-form").addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   greet();
+  // });
 });
