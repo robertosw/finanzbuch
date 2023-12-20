3a4b1d873191caf1e7efc785d7fc2e14ea647d31
Robert Oswald <robert.oswald.osw@gmail.com>
2023-12-20 21:12:26 +0000

    started depot_overview_alltime_get_data()

--- tauri/src-tauri/src/investing/depot_overview.rs
+++ tauri/src-tauri/src/investing/depot_overview.rs
@@ -3,14 +3,12 @@ use std::time::SystemTime;
 use chrono::DateTime;
 use chrono::Datelike;
 use chrono::Utc;
+use finanzbuch_lib::datafile;
+use finanzbuch_lib::investing::depot_entry;
 
 use crate::DATAFILE_GLOBAL;
 
-#[tauri::command]
-/// What this will look like:
-/// ['2023-01', '2023-02', '2023-03', '2023-04', '2023-05', '2023-06']
-pub fn depot_overview_alltime_get_labels() -> Vec<String>
-{
+fn _get_oldest_year_in_depot() -> Option<u16> {
     let datafile = DATAFILE_GLOBAL.lock().expect("DATAFILE_GLOBAL Mutex was poisoned");
 
     // go through all DepotEntries and see what the oldest month and year with values are
@@ -19,6 +17,8 @@ pub fn depot_overview_alltime_get_labels() -> Vec<String>
         .depot
         .values()
         .fold(u16::MAX, |accumulator_oldest_year: u16, de: &finanzbuch_lib::DepotEntry| {
+            // Go through all depot entries and note the oldest year in the accumulator
+            // The start value is just there to have a value in the variable, if there are no depot entries
             match de.history.first_key_value() {
                 Some((year, _)) => accumulator_oldest_year.min(*year),
                 None => accumulator_oldest_year,
@@ -26,8 +26,27 @@ pub fn depot_overview_alltime_get_labels() -> Vec<String>
         });
 
     if oldest_year == u16::MAX {
-        todo!("All depot entries have no history so there is no data, but this warning has to be implemented");
+        return None;
     }
+
+    return Some(oldest_year);
+}
+
+#[tauri::command]
+/// What this will look like:
+/// ['2023-01', '2023-02', '2023-03', '2023-04', '2023-05', '2023-06']
+pub fn depot_overview_alltime_get_labels() -> Vec<String>
+{
+    let datafile = DATAFILE_GLOBAL.lock().expect("DATAFILE_GLOBAL Mutex was poisoned");
+
+    
+    let oldest_year: u16 = match _get_oldest_year_in_depot() {
+        Some(y) => y,
+        None => todo!("All depot entries have no history so there is no data, but this warning has to be implemented"), // TODO
+    };
+
+    // Because every year that is created has all values set to 0, or changed by the user,
+    // its fair to assume that data for every month, starting from the oldest, exists
 
     // Now build a label for each month and year from oldest_year until today
     let now = SystemTime::now();
@@ -45,13 +64,36 @@ pub fn depot_overview_alltime_get_labels() -> Vec<String>
 #[tauri::command]
 /// The y-datapoints corresponding to the x-labels
 /// [6, 8, 3, 5, 2, 3]
-pub fn depot_overview_alltime_get_data() -> Vec<u32> { return vec![6, 8, 3, 5, 2, 3]; }
+pub fn depot_overview_alltime_get_data() -> Vec<f32>
+{
+    // TODO
+    // Add up all values of all depot entries for each month
+    
+    let datafile = DATAFILE_GLOBAL.lock().expect("DATAFILE_GLOBAL Mutex was poisoned");
+
+    let oldest_year: u16 = match _get_oldest_year_in_depot() {
+        Some(y) => y,
+        None => todo!("All depot entries have no history so there is no data, but this warning has to be implemented"), // TODO
+    };
+
+    // TODO it would be nice, that its guaranteed, that all depot entries have the same years
+
+    let mut values: Vec<f32> = Vec::new();
+
+    for depot_entry in datafile.investing.depot.values() {
+        // TODO
+    }
+
+
+    return vec![6.0, 8.0, 3.0, 5.0, 2.0, 3.0];
+}
 
 #[tauri::command]
 /// Given a growth rate of 7%, this will get the very first depot value and calculate all
 /// y-values for the x-labels up until the latest depot value
 pub fn depot_overview_alltime_get_prognosis(growth_rate: f32) -> Vec<f32>
 {
+    // TODO
     if growth_rate == 0.07 {
         return vec![6.0, 6.42, 6.8694, 7.350258, 7.86477606, 8.415310384];
     } else {
